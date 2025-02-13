90f7f6febafb78738624e2de24533388530742e9
AchrafBensalem05 <ac.bensalem@esi-sba.dz>
2025-02-13 10:42:40 +0100

    fixed layout with page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
-import Navbar from "./components/Navbar"; // Import Navbar
-import HeroSection from "./components/HeroSection";
+import Navbar from "@/components/Navbar"; // Import Navbar
+import HeroSection from "@/components/HeroSection";
 
 export const metadata: Metadata = {
   title: "Tech360 Client",
   description: "A Next.js website with Tailwind and a Navbar",
-  viewport: "width=device-width, initial-scale=1.0",
+};
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
 };
 
 export default function RootLayout({
@@ -19,7 +22,7 @@ export default function RootLayout({
       <body className="bg-white flex flex-col text-gray-900">
         <Navbar /> {/* Add Navbar here */}
         {/* <HeroSection /> */}
-        <main className="container max-w-7xl mx-auto p-6">{children}</main>
+        <main className="container w-full max-w-7xl mx-auto p-6">{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import HeroCarousel from "./components/HeroCarousel";
+import HeroCarousel from "@/components/HeroCarousel";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
