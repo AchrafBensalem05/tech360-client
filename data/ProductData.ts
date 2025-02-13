0740eca244046491846cce425b983de8a998c286
AchrafBensalem05 <ac.bensalem@esi-sba.dz>
2025-02-13 10:44:44 +0100

    added static product data

--- data/ProductData.ts
+++ data/ProductData.ts
@@ -1,77 +1,90 @@
-export interface Product {
-    id: number;
-    name: string;
-    price: number;
-    oldPrice?: number;
-    bestseller?: boolean;
-    rating?: string;
-    reviews?: number;
-    image: string;
+import { Product } from "@/types/product";
+
+export const productData: Product[] = [
+  {
+    id: 1,
+    name: "Galaxy S21 FE 5G 128GB - Gray - Unlocked",
+    price: 139.09,
+    oldPrice: 699.99,
+    discount: 560.90,
+    bestseller: true,
+    rating: 4.1,
+    reviews: 137,
+    image: "/images/product1.jpeg",
+    link: "/products/1",
+    details: {
+      delivery: "Free delivery by Feb 13 - Feb 14",
+      expressDelivery: "Express delivery by Feb 12 - Feb 13 from $25.00",
+      warranty: "Free 30-day returns • 1-year warranty",
+      carrierSupport: "Works with all carriers",
+      refurbished: "Verified Refurbished in the US",
+    }
+  },
+  {
+    id: 2,
+    name: "Wyze WLPA19C lighting",
+    price: 20.03,
+    oldPrice: 40.48,
+    discount: 20.45,
+    bestseller: true,
+    rating: 5,
+    reviews: 3,
+    image: "/images/product2.jpeg",
+    link: "/products/2"
+  },
+  {
+    id: 3,
+    name: "Hatch Restore 2 Night light",
+    price: 118.99,
+    oldPrice: 199.99,
+    discount: 81.00,
+    bestseller: true,
+    rating: 4.9,
+    reviews: 2,
+    image: "/images/product3.jpg",
+    link: "/products/3"
+  },
+  {
+    id: 4,
+    name: "Galaxy S21 FE 5G 128GB - Gray - Unlocked",
+    price: 139.09,
+    oldPrice: 699.99,
+    discount: 560.90,
+    bestseller: true,
+    rating: 4.1,
+    reviews: 137,
+    image: "/images/product1.jpeg",
+    link: "/products/1",
+    details: {
+      delivery: "Free delivery by Feb 13 - Feb 14",
+      expressDelivery: "Express delivery by Feb 12 - Feb 13 from $25.00",
+      warranty: "Free 30-day returns • 1-year warranty",
+      carrierSupport: "Works with all carriers",
+      refurbished: "Verified Refurbished in the US",
+    }
+  },
+  {
+    id: 5,
+    name: "Wyze WLPA19C lighting",
+    price: 20.03,
+    oldPrice: 40.48,
+    discount: 20.45,
+    bestseller: true,
+    rating: 5,
+    reviews: 3,
+    image: "/images/product2.jpeg",
+    link: "/products/2"
+  },
+  {
+    id: 6,
+    name: "Hatch Restore 2 Night light",
+    price: 118.99,
+    oldPrice: 199.99,
+    discount: 81.00,
+    bestseller: true,
+    rating: 4.9,
+    reviews: 2,
+    image: "/images/product3.jpg",
+    link: "/products/3"
   }
-  
-  export const productData: Product[] = [
-    {
-      id: 1,
-      name: "Hatch Rest 2ND Gen Night light",
-      price: 38.49,
-      oldPrice: 69.99,
-      bestseller: true,
-      image: "/images/product1.jpeg",
-    },
-    {
-      id: 2,
-      name: "Wyze WLPA19C lighting",
-      price: 20.03,
-      oldPrice: 40.48,
-      bestseller: true,
-      rating: "5/5",
-      reviews: 3,
-      image: "/images/product2.jpeg",
-    },
-    {
-      id: 3,
-      name: "Hatch Restore 2 Night light",
-      price: 118.99,
-      oldPrice: 199.99,
-      bestseller: true,
-      rating: "4.9/5",
-      reviews: 2,
-      image: "/images/product3.jpg",
-    },
-    {
-      id: 4,
-      name: "Ninja Nc301 Creami Ice cream maker",
-      price: 290.0,
-      oldPrice: 359.99,
-      image: "/images/product4.jpeg",
-    },
-    {
-        id: 5,
-        name: "Hatch Rest 2ND Gen Night light",
-        price: 38.49,
-        oldPrice: 69.99,
-        bestseller: true,
-        image: "/images/product1.jpeg",
-      },
-      {
-        id: 6,
-        name: "Wyze WLPA19C lighting",
-        price: 20.03,
-        oldPrice: 40.48,
-        bestseller: true,
-        rating: "5/5",
-        reviews: 3,
-        image: "/images/product2.jpeg",
-      },
-      {
-        id: 7,
-        name: "Hatch Restore 2 Night light",
-        price: 118.99,
-        oldPrice: 199.99,
-        bestseller: true,
-        rating: "4.9/5",
-        reviews: 2,
-        image: "/images/product3.jpg",
-      }
-  ];
-  
+];
